66e17417557f4117468f13d1b8ba23b4279c2834
Narinder <narinder.mehra@pharmeasy.in>
2019-04-08 11:19:49 +0530

    First commit

---
+++ src/test/kotlin/com/microservices/reactiveservice/ReactiveServiceApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.microservices.reactiveservice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ReactiveServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
