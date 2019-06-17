a642eafed15be72de2504bc16e63f486a5b7b3b6
Narinder <narinder.mehra@pharmeasy.in>
2019-06-17 17:04:26 +0530

    First commit

---
+++ src/test/kotlin/com/microservices/zuulservice/ZuulServiceApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.microservices.zuulservice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
